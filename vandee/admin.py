07105fde1ecff9fc21a553e1674a7bba4239b1bc
remanzy <wowrehman@gmail.com>
2023-01-27 05:23:46 +0500

    'message'

--- vandee/admin.py
+++ vandee/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import VandeeDetails
+# from .models import VandeeCdo
+# from .models import VandeeVdo
+# from .models import VandeeVerifyInvoice
+
+
+admin.site.register(VandeeDetails)
--- vandee/models.py
+++ vandee/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+class VandeeDetails(models.Model):
+
+    user_type = models.TextChoices('VANDEE', 'VENDOR')
+    vandee_id = models.AutoField(primary_key=True)
+    vandee_realName = models.CharField(max_length=40)
+    vandee_email = models.EmailField(max_length=30)
+    vandee_phone = models.IntegerField(max_length=15)
+
+
+
+    def __str__(self):
+        return self.name
+
+class VandeeCdo(models.Model):
+
+    item_no = models.IntegerField(max_length=500)
+    item_name = models.CharField(max_length=40)
+    quantity = models.IntegerField(max_length=800)
+
+    def __str__(self):
+        return self.name
+
+class VandeeVdo(models.Model):
+
+    delivery_orderId = models.AutoField(primary_key=True)
+    item_no = models.IntegerField(max_length=500)
+    item_name = models.CharField(max_length=40)
+    quantity = models.IntegerField(max_length=800)
+
+    def __str__(self):
+        return self.name
+
+class VandeeVerifyInvoice(models.Model):
+
+    Invoice_id = models.AutoField(primary_key=True)
+    item_types = models.IntegerField(max_length=500)
+    vandee_id = models.IntegerField(max_length=800)
+    verfication_status = models.CharField(max_length=40)
+
+
+    def __str__(self):
+        return self.name
+
